050830030ea55722029eb2686c6845e440bed442
NicoSciangula <nico.sciangula99@gmail.com>
2026-03-02 21:16:28 +0100

    added useState for component FilmList

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,10 +3,27 @@ import Form from "./Form";
 import { useState, useEffect } from "react";
 
 export default function Main() {
+  const [isOpen, setisOpen] = useState(false);
+
+  function openDropDown() {
+    setisOpen((current) => !current);
+  }
+
   return (
     <>
       <main className="container">
-        <FilmList />
+        <FilmList openDrop={openDropDown} state={isOpen} />
+        <div className="mb-3">
+          <label for="formGroupExampleInput" className="form-label">
+            <b>Filtra genere</b>
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="formGroupExampleInput"
+            placeholder="Inserisci genere del film"
+          />
+        </div>
       </main>
     </>
   );
