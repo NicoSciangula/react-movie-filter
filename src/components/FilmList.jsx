3b9947218ecec7f540dce0657a6c2dbd9c6194ed
NicoSciangula <nico.sciangula99@gmail.com>
2026-03-02 21:35:48 +0100

    removed array with movie list and used the props passed to it

--- src/components/FilmList.jsx
+++ src/components/FilmList.jsx
@@ -1,46 +1,12 @@
 import FilmCard from "./products/FilmCard";
 
-const films = [
-  {
-    title: "Inception",
-    genre: "Fantascienza",
-    image:
-      "https://stanzedicinema.com/wp-content/uploads/2010/05/inception-poster4.jpg?w=691",
-  },
-  {
-    title: "Il Padrino",
-    genre: "Thriller",
-    image: "https://pad.mymovies.it/filmclub/2002/08/056/locandina.jpg",
-  },
-  {
-    title: "Titanic",
-    genre: "Romantico",
-    image: "https://pad.mymovies.it/filmclub/2004/06/506/locandinapg9.jpg",
-  },
-  {
-    title: "Batman",
-    genre: "Azione",
-    image: "https://static.posters.cz/image/1300/198201.jpg",
-  },
-  {
-    title: "Interstellar",
-    genre: "Fantascienza",
-    image: "https://pad.mymovies.it/filmclub/2014/01/001/locandina.jpg",
-  },
-  {
-    title: "Pulp Fiction",
-    genre: "Thriller",
-    image: "https://static.posters.cz/image/1300/1288.jpg",
-  },
-];
-
-export default function FilmList({ openDrop, state }) {
+export default function FilmList({ listFilm }) {
   return (
     <div className="my-3">
       <h1 className="text-center">Lista Film</h1>
       <div>
         <ul className="list-unstyled d-flex flex-wrap">
-          {films.map((singleElement, index) => (
+          {listFilm.map((singleElement, index) => (
             <li key={index} className="p-3">
               <FilmCard film={singleElement} />
             </li>
@@ -51,7 +17,7 @@ export default function FilmList({ openDrop, state }) {
           aria-label="Large select example"
         >
           <option selected>Genere</option>
-          {films.map((singleFilm, index) => (
+          {listFilm.map((singleFilm, index) => (
             <option key={index} value={index}>
               {singleFilm.genre}
             </option>
