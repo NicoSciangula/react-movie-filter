08cdb38df32b890ef89988f28889f7b2da6010bc
NicoSciangula <nico.sciangula99@gmail.com>
2026-03-03 21:52:34 +0100

    created form to add new film card

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,51 @@
-export default function Form(){
-    
+export default function Form({ inputChange, val, pushFilm }) {
+  const { filmTitle, filmGenre, filmImage } = val;
+  return (
+    <>
+      <form onSubmit={pushFilm}>
+        <div>
+          <label htmlFor="title" className="form-label">
+            Titolo Film
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            name="filmTitle"
+            value={filmTitle}
+            onChange={inputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="genre" className="form-label">
+            Genere del Film
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="genre"
+            name="filmGenre"
+            value={filmGenre}
+            onChange={inputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="img" className="form-label">
+            Copertina
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="img"
+            name="filmImage"
+            value={filmImage}
+            onChange={inputChange}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary mt-3">
+          Inserisci
+        </button>
+      </form>
+    </>
+  );
 }
